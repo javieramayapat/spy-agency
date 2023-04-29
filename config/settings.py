c061591bf09c58df3c2f7bec6fcfc37d50c61bfe
Javier Amaya <Javieramayapat@gmail.com>
2023-04-29 14:35:35 -0600

    feat[custom-user]: Add custom user mode, normalize email and handle encripting for password

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# overview of the user
+AUTH_USER_MODEL = "core.User"
