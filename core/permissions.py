0545c3f1b39820fd3e630172a54c0843dd048ad6
Javier Amaya <Javieramayapat@gmail.com>
2023-05-03 11:38:56 -0600

    feat[permissions]: Add permision IsManager and IsHitman

--- core/permissions.py
+++ core/permissions.py
@@ -11,3 +11,15 @@ class IsBigBoss(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         return user.is_authenticated and user.id == 1
+
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.is_authenticated and user.is_manager
+
+
+class IsHitman(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.is_authenticated and user.is_hitman
